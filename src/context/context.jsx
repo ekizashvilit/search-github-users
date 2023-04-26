84a3b698c7bf87c393dd7aeaa868d82a57a4ecd3
Teona <tekizashvili@gmail.com>
2023-04-26 10:19:17 +0400

    set up mock data

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,11 +1,22 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
 
 const rootUrl = 'https://api.github.com';
 
 export const GithubContext = createContext();
 
 export const GithubProvider = ({ children }) => {
-  return <GithubContext.Provider value="hi">{children}</GithubContext.Provider>;
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
