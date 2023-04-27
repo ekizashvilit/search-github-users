7528bcf68b5a349cb00125fc49548cc7cae9959a
Teona <tekizashvili@gmail.com>
2023-04-27 18:41:23 +0400

    added card component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,4 +1,25 @@
+import styled from 'styled-components';
+import Cards from './Cards';
+import Followers from './Followers';
+
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Cards />
+        <Followers />
+      </Wrapper>
+    </section>
+  );
 };
+
+const Wrapper = styled.section`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
 export default User;
