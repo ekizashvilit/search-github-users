81c6555ee49ae65a40e28e2f7af27485dd47281d
Teona <tekizashvili@gmail.com>
2023-04-30 11:07:18 +0400

    set up doughnut chart

--- src/components/charts/Doughnut2D.jsx
+++ src/components/charts/Doughnut2D.jsx
@@ -1,4 +1,32 @@
-const Doughnut2D = () => {
-  return <div>Doughnut2D</div>;
+import ReactFC from 'react-fusioncharts';
+
+import FusionCharts from 'fusioncharts';
+
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
+
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'doughnut2d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: 'Stars Per Language',
+        theme: 'candy',
+        decimals: 0,
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+      },
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
-export default Doughnut2D;
+
+export default ChartComponent;
