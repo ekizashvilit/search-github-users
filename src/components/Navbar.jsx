29a6c2345a8cf80b4f412de69160eb1de3dcf4e7
Teona <tekizashvili@gmail.com>
2023-05-03 10:52:33 +0400

    added login/logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
 import styled from 'styled-components';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
+
+  const isUser = isAuthenticated && user;
+
+  return (
+    <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt={user.name}></img>}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.nav`
@@ -14,16 +45,20 @@ const Wrapper = styled.nav`
   justify-content: center;
   align-items: center;
   gap: 1.5rem;
+  position: relative;
+
   h4 {
     margin-bottom: 0;
     font-weight: 400;
   }
+
   img {
     width: 35px !important;
     height: 35px;
     border-radius: 50%;
     object-fit: cover;
   }
+
   button {
     background: transparent;
     border: transparent;
